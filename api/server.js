12d1aa6d629da54c1e06a4644ed4b12708c8da5c
akashperumal <akashperumal2003@gmail.com>
2024-10-24 23:01:29 +0530

    Update backend with vercel 8

--- api/server.js
+++ api/server.js
@@ -758,8 +758,5 @@ const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 module.exports = app;
